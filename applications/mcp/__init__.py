186baea5d991adec50e664161f5061e0713f132e
Mithilesh1103git <mithileshnakade1103@gmail.com>
2026-02-16 15:14:13 +0530

    mcp path changes

--- applications/mcp/__init__.py
+++ applications/mcp/__init__.py
@@ -9,7 +9,7 @@ print(os.getenv("MCP_SERVER_HOST"), os.getenv("MCP_SERVER_PORT"))
 MCP_SERVER_HOST = os.getenv("MCP_SERVER_HOST")
 MCP_SERVER_PORT = os.getenv("MCP_SERVER_PORT", "8080")
 
-app = mcp_app.http_app()
+app = mcp_app.http_app(path="/sse", transport="sse")
 
-if __name__ == "__main__":
-    mcp_app.run(transport="sse", host="0.0.0.0", port=int(MCP_SERVER_PORT))
+# if __name__ == "__main__":
+#     mcp_app.run(transport="sse", host="0.0.0.0", port=int(MCP_SERVER_PORT))
